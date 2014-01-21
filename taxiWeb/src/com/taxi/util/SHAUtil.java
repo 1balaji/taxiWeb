595b9a442f0007c59f56530412947a03402cfc3c
giorgiarchva <g_archvadze@hotmail.com>
2014-01-22 00:14:07 +0700

    Added SHA2 String Hashing Function

--- taxiWeb/src/com/taxi/util/SHAUtil.java
+++ taxiWeb/src/com/taxi/util/SHAUtil.java
@@ -1,16 +1,22 @@
 package com.taxi.util;
 
-public class SHAUtil 
-{
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-	
-	public static String getSHA2(String str)
-	{
-		String hash="";
-		
-		
-		return hash;
+public class SHAUtil {
+
+	public static String getSHA2(String str) throws NoSuchAlgorithmException {
+
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update(str.getBytes());
+		return bytesToHex(md.digest());
 	}
-	
-	
+
+	public static String bytesToHex(byte[] bytes) {
+		StringBuffer result = new StringBuffer();
+		for (byte byt : bytes)
+			result.append(Integer.toString((byt & 0xff) + 0x100, 16).substring(1));
+		return result.toString();
+	}
+
 }
