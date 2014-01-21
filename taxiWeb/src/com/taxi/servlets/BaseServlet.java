687cd27c6a08603899b8565574bae0f8d9c9bea2
iraklivasha <irakli.vashakidze@yahoo.com>
2014-01-21 18:25:59 +0400

    added FilterBase

--- taxiWeb/src/com/taxi/servlets/BaseServlet.java
+++ taxiWeb/src/com/taxi/servlets/BaseServlet.java
@@ -2,7 +2,6 @@ package com.taxi.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -31,6 +30,7 @@ public class BaseServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
@@ -41,5 +41,6 @@ public class BaseServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
+
 
 }
