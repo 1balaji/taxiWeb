7146f9b82ff1d42b6ad2452022dc12f8ac2b0115
iraklivasha <irakli.vashakidze@yahoo.com>
2014-01-22 19:24:44 +0400

    fixed project warnings

--- taxiWeb/src/com/taxi/servlets/RegistratorServlet.java
+++ taxiWeb/src/com/taxi/servlets/RegistratorServlet.java
@@ -1,16 +1,10 @@
 package com.taxi.servlets;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.taxi.util.SHAUtil;
 
 /**
  * Servlet implementation class RegistratorServlet
