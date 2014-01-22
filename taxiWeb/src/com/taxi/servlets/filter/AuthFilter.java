f97a6efacd8c36720b80bace7654f2d223ab8838
iraklivasha <irakli.vashakidze@yahoo.com>
2014-01-22 19:25:18 +0400

    Fixed project warnings

--- taxiWeb/src/com/taxi/servlets/filter/AuthFilter.java
+++ taxiWeb/src/com/taxi/servlets/filter/AuthFilter.java
@@ -3,14 +3,11 @@ package com.taxi.servlets.filter;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONException;
