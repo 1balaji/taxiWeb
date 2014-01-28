7e584b233037454353738dc21dd7679679549745
giorgiarchva <g_archvadze@hotmail.com>
2014-01-29 01:11:43 +0700

    Added some json parser parameters

--- taxiWeb/src/com/taxi/servlets/RegistratorServlet.java
+++ taxiWeb/src/com/taxi/servlets/RegistratorServlet.java
@@ -27,6 +27,15 @@ public class RegistratorServlet extends GenericServlet {
 	private static final String KEY_SYSVERSION = "systemVersion";
 	private static final String KEY_DEVICEMODEL = "deviceModel";
 	private static final String KEY_REGDATA = "registrationData";
+	private static final String KEY_REGDATA_USERNAME = "username";
+	private static final String KEY_REGDATA_PASSWORD = "password";
+	private static final String KEY_REGDATA_NAME = "name";
+	private static final String KEY_REGDATA_SURNAME = "surname";
+	private static final String KEY_REGDATA_EMAIL = "email";
+	private static final String KEY_REGDATA_MOBILE = "mobile";
+	private static final String KEY_REGDATA_NOTE = "note";
+	private static final String KEY_REGDATA_LANGUAGE = "language";
+	
 	
     /**
      * @see HttpServlet#HttpServlet()
@@ -62,10 +71,22 @@ public class RegistratorServlet extends GenericServlet {
 			String deviceModel = this.jsonObj.getString(KEY_DEVICEMODEL);
 			String systemVersion = this.jsonObj.getString(KEY_SYSVERSION);
 			JSONObject registrationData = this.jsonObj.getJSONObject(KEY_REGDATA);
+			String systemUsername = registrationData.getString(KEY_REGDATA_USERNAME);
+			String systemPassword = registrationData.getString(KEY_REGDATA_PASSWORD);
+			String systemName = registrationData.getString(KEY_REGDATA_NAME);
+			String systemSurname = registrationData.getString(KEY_REGDATA_SURNAME);
+			String systemEmail = registrationData.getString(KEY_REGDATA_EMAIL);
+			String systemMobile = registrationData.getString(KEY_REGDATA_MOBILE);
+			String systemNote = registrationData.getString(KEY_REGDATA_NOTE);
+			String systemLanguage = registrationData.getString(KEY_REGDATA_LANGUAGE);
+			
+			
 			
 			System.out.println(deviceUID);
 			System.out.println(deviceModel);
 			System.out.println(systemVersion);
+			
+		
 			
 		} catch (JSONException e) {
 			Logger.logError(String.format("Failed to parse json, %s", e.getMessage()));
